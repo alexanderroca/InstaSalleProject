4f6002916a36ab539604f0f082256ebfbc708927
alexanderroca <alexander.roca@students.salle.url.edu>
2019-04-11 22:39:16 +0200

    FEAT: Trie

---
+++ src/structures/Trie/TrieNode.java
@@ -0,0 +1,29 @@
+package structures.Trie;
+
+public class TrieNode {
+    private boolean isEndOfWord;
+    private TrieNode[] sons;
+
+    public TrieNode(){
+        sons = new TrieNode[Trie.ALPHABET];
+        isEndOfWord = false;
+        for(int i = 0; i < Trie.ALPHABET; i++)
+            sons[i] = null;
+    }
+
+    public boolean isEndOfWord() {
+        return isEndOfWord;
+    }
+
+    public void setEndOfWord(boolean endOfWord) {
+        this.isEndOfWord = endOfWord;
+    }
+
+    public TrieNode[] getSons() {
+        return sons;
+    }
+
+    public void setSons(TrieNode[] sons) {
+        this.sons = sons;
+    }
+}
