0bba250b3945c3bd2e0b2b1de39b688c73477725
alexanderroca <alexander.roca@students.salle.url.edu>
2019-04-23 13:12:28 +0200

    DOC: AVLTree, NodeAVL

--- src/structures/AVLTree/AVLTree.java
+++ src/structures/AVLTree/AVLTree.java
@@ -3,29 +3,50 @@ package structures.AVLTree;
 import structures.NodeAlreadyExists;
 import structures.NodeDoesntExists;
 
+/**
+ * Classe amb les operacions basiques del arbre binari AVL
+ * @author Alexander Roca
+ * @version 0.1
+ */
 public class AVLTree{
     private NodeAVL root;
 
-    // Function to get the height of the tree
+    /**
+     * Funcio que obte l'alçada del arbre a partir d'un node
+     * @param n : NodeAVL
+     * @return int
+     */
     public int height(NodeAVL n){
         if(n == null)
             return 0;
         return n.getHeight();
     }
 
-    // Get the maximum of two integers
+    /**
+     * Funcio que indica quin dels dos nombres es major
+     * @param a : int
+     * @param b : int
+     * @return int
+     */
     public int max(int a, int b){
-        return (a > b) ? a : b;
+        return (a > b) ? a : b; // Equivalent a fer if i else (es una ternaria)
     }
 
-    // Get the Balance Factor of NodeAVL n
+    /**
+     * Funcio que obte el factor de balanceig a partir d'un node
+     * @param n : NodeAVL
+     * @return int
+     */
     public int getBalance(NodeAVL n){
         if(n == null)
             return 0;
         return (height(n.getLeft()) - height(n.getRight()));
     }
 
-    // PreOrder
+    /**
+     * Procediment que mostra els elements de l'arbre amb els criteris de PreOrder
+     * @param n : NodeAVL
+     */
     public void preOrder(NodeAVL n){
         if(n == null)
             return;
@@ -35,6 +56,11 @@ public class AVLTree{
     }
 
     // InOrder
+
+    /**
+     * Procediment que mostra els elements de l'arbre amb els criteris de InOrder
+     * @param n : NodeAVL
+     */
     public void inOrder(NodeAVL n){
         if(n == null)
             return;
@@ -44,6 +70,11 @@ public class AVLTree{
     }
 
     //PostOrder
+
+    /**
+     * Procediment que mostra els elements de l'arbre amb els criteris de PostOrder
+     * @param n : NodeAVL
+     */
     public void postOrder(NodeAVL n){
         if(n == null)
             return;
@@ -52,35 +83,48 @@ public class AVLTree{
         System.out.print(n.getKey() + " ");
     }
 
-    // LL ROTATION
+    /**
+     * Funcio que realitza la rotacio de tipus LL quan es produeix un desbalanceig en l'arbre
+     * @param n : NodeAVL
+     * @return NodeAVL
+     */
     public NodeAVL rightRotate(NodeAVL n){
-        NodeAVL left = n.getLeft();
-        NodeAVL left_right = left.getRight();
+        NodeAVL left = n.getLeft(); //Obtenim el fill esquerre
+        NodeAVL left_right = left.getRight();   //Obtenim el fill dret del fill esquerre
 
-        left.setRight(n);
-        n.setLeft(left_right);
+        left.setRight(n);   //Sobreescrivim el fill dret amb el node que produeix el desbalanceig
+        n.setLeft(left_right);  //Sobreescrivim el node on s'ha produit el desbalanceig com ha fill dret del fill esquerre
 
-        n.setHeight(max(height(n.getLeft()), height(n.getRight())) + 1);
-        left.setHeight(max(height(left.getLeft()), height(left.getRight())) + 1);
+        n.setHeight(max(height(n.getLeft()), height(n.getRight())) + 1);    //Recalculem la nova alçada del node actual
+        left.setHeight(max(height(left.getLeft()), height(left.getRight())) + 1);   //Recalculem ka nova alçada del fill esquerre
 
         return left;
     }
 
-    // RR ROTATION
+    /**
+     * Funcio que realitza la rotacio de tipus RR quan es produeix un desbalanceig en l'arbre
+     * @param n : NodeAVL
+     * @return NodeAVL
+     */
     public NodeAVL leftRotate(NodeAVL n){
-        NodeAVL right = n.getRight();
-        NodeAVL right_left = right.getLeft();
+        NodeAVL right = n.getRight();   //Obtenim el fill dret
+        NodeAVL right_left = right.getLeft();   //Obtenim el fill esquerre del fill dret
 
-        right.setLeft(n);
-        n.setRight(right_left);
+        right.setLeft(n);   //Sobreescrivim el node esquerre amb el node que produeix el desbalanceig
+        n.setRight(right_left); //Sobreescrivim el node on s'ha produit el desbalanceig com a fill esquerre del fill dret
 
-        n.setHeight(max(height(n.getLeft()), height(n.getRight())) + 1);
-        right.setHeight(max(height(right.getLeft()), height(right.getRight())) + 1);
+        n.setHeight(max(height(n.getLeft()), height(n.getRight())) + 1);    //Recalculem la nova alçada del node actual
+        right.setHeight(max(height(right.getLeft()), height(right.getRight())) + 1);    //Recalculem ka nova alçada del fill dret
 
         return right;
     }
 
-
+    /**
+     * Funcio que comprova si l'element a inserir no existeixi previament
+     * @param key : int
+     * @param object : Object
+     * @return boolean
+     */
     public boolean insert_T(int key, Object object){
 
         try {
@@ -92,6 +136,11 @@ public class AVLTree{
         }
     }
 
+    /**
+     * Funcio que comprova si l'element a eliminarexisteix o no
+     * @param key : int
+     * @return boolean
+     */
     public boolean remove_T(int key){
         try{
             root = remove(root, key);
@@ -102,17 +151,27 @@ public class AVLTree{
         }
     }
 
+    /**
+     * Funcio que elimina l'element de l'estructura
+     * @param n : NodeAVL
+     * @param key : int
+     * @return NodeAVL
+     * @throws NodeDoesntExists
+     */
     public NodeAVL remove(NodeAVL n, int key) throws NodeDoesntExists{
 
+        //Cas de que el node que busquem sigui null
         if(n == null)
             throw new NodeDoesntExists();
 
+        //Recorrem l'arbre fins trobar el node a eliminar
         if(key < n.getKey())
             n.setLeft(remove(n.getLeft(), key));
         else if(key > n.getKey())
             n.setRight(remove(n.getRight(), key));
+        //Cas que cal eliminar l'element
         else{
-
+            //Cas que el node a eliminar tingui 0 fills o 1 fill
             if(n.getLeft() == null || n.getRight() == null){
 
                 NodeAVL aux = null;
@@ -121,35 +180,42 @@ public class AVLTree{
                 else
                     aux = n.getLeft();
 
+                //Cas de 1 fill
                 if(aux == null){
                     aux = n;
                     n = null;
                 }   //if
+                //Cas de 0 fills
                 else
                     n = aux;
             }   //if
+            //Cas de 2 fills
             else{
-                NodeAVL aux = minValueNode(n.getRight());
-                n.setKey(aux.getKey());
-                n.setRight(remove(n.getRight(), aux.getKey()));
+                NodeAVL aux = minValueNode(n.getRight());   //Realitzem un inOrder local, el successor sera el de l'esquerra
+                n.setKey(aux.getKey()); //Copiem el successor del InOrder local al node actual
+                n.setRight(remove(n.getRight(), aux.getKey())); //Eliminem el node successor de la seva antiga posicio
             }   //else
         }   //else
 
+        //Cas en que l'arbre nomes tingues 1 node que retornar
         if(n == null)
             return n;
 
-        n.setHeight(max(height(n.getLeft()), height(n.getRight())) + 1);
+        n.setHeight(max(height(n.getLeft()), height(n.getRight())) + 1);    //Actualitzem l'alçada del node actual
 
-        int balance = getBalance(n);
+        int balance = getBalance(n);    //Comprovem que no hi hagi desbalanceig
+        //LL
         if(balance > 1 && getBalance(n.getLeft()) >= 0)
             return rightRotate(n);
+        //LR
         if(balance > 1 && getBalance(n.getLeft()) < 0){
             n.setLeft(leftRotate(n.getLeft()));
             return rightRotate(n);
         }   //if
-
+        //RR
         if(balance < -1 && getBalance(n.getRight()) <= 0)
             return leftRotate(n);
+        //RL
         if(balance < -1 && getBalance(n.getRight()) > 0) {
             n.setRight(rightRotate(n.getRight()));
             return leftRotate(n);
@@ -158,22 +224,34 @@ public class AVLTree{
         return n;
     }
 
-    //Search the minimum key value in the tree
+    /**
+     * Funcio que cerca l'identificador unic mes petit de l'arbre
+     * @param n : NodeAVL
+     * @return NodeAVL
+     */
     public NodeAVL minValueNode(NodeAVL n){
         NodeAVL aux = n;
 
-        //Loop to find the most left leaf
+        //Bucle per trobar la fulla que esta més a l'esquerra
         while(aux.getLeft() != null)
             aux = aux.getLeft();
 
         return aux;
     }
 
-    // Insertion of a new NodeAVL to the AVL Tree
+    /**
+     * Funcio que insereix un nou element a l'estructura
+     * @param n : NodeAVL
+     * @param key : int
+     * @param object : Object
+     * @return NodeAVL
+     * @throws NodeAlreadyExists
+     */
     public NodeAVL insert(NodeAVL n, int key, Object object) throws NodeAlreadyExists{
 
+        //Inserim el nou element
         if (n == null)
-            return (new NodeAVL(key, object)); //TODO: Determine what type of object to keep
+            return (new NodeAVL(key, object));
 
         else {
             if (n.getKey() == key)
@@ -186,16 +264,17 @@ public class AVLTree{
             else if (key > n.getKey()) {
                 n.setRight(insert(n.getRight(), key, object));
             }   //else-if
+            // Identificadors unics no son valids
             else {
                 return n;
             }   //else
 
-            // Update heights
+            // Actualitzem les noves alçades
             n.setHeight(1 + max(height(n.getLeft()), height(n.getRight())));
 
-            // Check if is balanced with the Balance Factor
+            // Comprova si el node esta balancejat
             int balance = getBalance(n);
-            // 4 possibles types of unbalanced
+            // 4 possibles tipus de desbalanceig
             //RR
             if (balance > 1 && key < n.getLeft().getKey())
                 return rightRotate(n);
--- src/structures/AVLTree/NodeAVL.java
+++ src/structures/AVLTree/NodeAVL.java
@@ -1,14 +1,24 @@
 package structures.AVLTree;
 
+/**
+ * Classe Node de l'arbre AVL
+ * @author Alexander Roca
+ * @version 0.1
+ */
 public class NodeAVL {
-    private int key;
-    private int height;
-    private NodeAVL right;
-    private NodeAVL left;
-    private NodeAVL parent;
-    private Object object;
+    private int key;    //identificador unic
+    private int height; //altura del l'arbre
+    private NodeAVL right;  //fill dret
+    private NodeAVL left;   //fill esquerra
+    private NodeAVL parent; //pare
+    private Object object;  //element
     private boolean active;
 
+    /**
+     * Constructor que necessita l'identificador unic i l'element que contindra
+     * @param key : int
+     * @param object : Object
+     */
     public NodeAVL(int key, Object object) {
         this.key = key;
         height = 1;
